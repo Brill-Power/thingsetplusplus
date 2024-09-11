b6d640b5be3de323b073b9dbcf3fbc29508f0caf
Gareth Potter <gareth.potter@brillpower.com>
2024-09-11 22:44:33 +0100

    fix test

--- tests/TestCompatibility.cpp
+++ tests/TestCompatibility.cpp
@@ -7,5 +7,5 @@ using namespace ThingSet;
 TEST(Compatibility, SimpleProperty)
 {
     float f;
-    THINGSET_ADD_ITEM_FLOAT(0x0, 0x100, "xFloat", &f, THINGSET_ANY_R | THINGSET_MFR_W, 0)
+    THINGSET_ADD_ITEM_FLOAT(0x0, 0x100, "xFloat", &f, 0, THINGSET_ANY_R | THINGSET_MFR_W, 0)
 }
