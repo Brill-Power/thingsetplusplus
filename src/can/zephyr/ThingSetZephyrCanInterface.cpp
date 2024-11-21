8838669233bcff1ec1c94fb394d28b680c095eb1
Gareth Potter <gareth.potter@brillpower.com>
2024-11-21 10:24:50 +0000

    Zephyr CAN: Zephyr 3.6 compatibility fixes

--- src/can/zephyr/ThingSetZephyrCanInterface.cpp
+++ src/can/zephyr/ThingSetZephyrCanInterface.cpp
@@ -4,9 +4,9 @@
  * SPDX-License-Identifier: Proprietary
  */
 #include "thingset++/can/zephyr/ThingSetZephyrCanInterface.hpp"
-#include "thingset++/can/zephyr/CanFrame.hpp"
 #include "thingset++/Eui.hpp"
 #include "thingset++/can/CanID.hpp"
+#include "thingset++/can/zephyr/CanFrame.hpp"
 #include "thingset++/internal/logging.hpp"
 #include <chrono>
 #include <string.h>
@@ -23,8 +23,7 @@ static const isotp_fast_opts flowControlOptions = {
     .addressing_mode = ISOTP_FAST_ADDRESSING_MODE_FIXED,
 };
 
-_ThingSetZephyrCanInterface::_ThingSetZephyrCanInterface(const device *const canDevice)
-    : _canDevice(canDevice)
+_ThingSetZephyrCanInterface::_ThingSetZephyrCanInterface(const device *const canDevice) : _canDevice(canDevice)
 {
     _nodeAddress = CanID::broadcastAddress;
 }
@@ -35,14 +34,14 @@ bool _ThingSetZephyrCanInterface::publish(CanID &id, uint8_t *buffer, size_t len
     memcpy(frame.getData(), buffer, length);
     frame.setLength(length);
     frame.setFd(true);
-    int result = can_send(_canDevice, frame.getFrame(), K_MSEC(CONFIG_THINGSET_CAN_REPORT_SEND_TIMEOUT), nullptr, nullptr);
+    int result =
+        can_send(_canDevice, frame.getFrame(), K_MSEC(CONFIG_THINGSET_CAN_REPORT_SEND_TIMEOUT), nullptr, nullptr);
     return result == 0;
 }
 
 ThingSetZephyrCanStubInterface::ThingSetZephyrCanStubInterface(const device *const canDevice)
     : _ThingSetZephyrCanInterface::_ThingSetZephyrCanInterface(canDevice)
-{
-}
+{}
 
 bool ThingSetZephyrCanStubInterface::bind(uint8_t nodeAddress)
 {
@@ -57,15 +56,15 @@ bool ThingSetZephyrCanStubInterface::listen(std::function<int(uint8_t *, size_t,
 
 ThingSetZephyrCanInterface::ThingSetZephyrCanInterface(const device *const canDevice)
     : _ThingSetZephyrCanInterface::_ThingSetZephyrCanInterface(canDevice)
-{
-}
+{}
 
 ThingSetZephyrCanInterface::~ThingSetZephyrCanInterface()
 {
     isotp_fast_unbind(&_listener.requestResponseContext);
 }
 
-void ThingSetZephyrCanInterface::onRequestResponseReceived(net_buf *buffer, int remainingLength, isotp_fast_addr address, void *arg)
+void ThingSetZephyrCanInterface::onRequestResponseReceived(net_buf *buffer, int remainingLength,
+                                                           isotp_fast_addr address, void *arg)
 {
     Listener *listener = (Listener *)arg;
     size_t len = net_buf_frags_len(buffer);
@@ -77,7 +76,8 @@ void ThingSetZephyrCanInterface::onRequestResponseReceived(net_buf *buffer, int 
     if (len > 0) {
 #pragma GCC diagnostic push
 #pragma GCC diagnostic ignored "-Wnarrowing"
-        int result = isotp_fast_send(&listener->requestResponseContext, txBuffer, len, isotp_fast_addr { .ext_id = CanID::create(address.ext_id).getReplyId() }, nullptr);
+        int result = isotp_fast_send(&listener->requestResponseContext, txBuffer, len,
+                                     isotp_fast_addr{ .ext_id = CanID::create(address.ext_id).getReplyId() }, nullptr);
 #pragma GCC diagnostic pop
         if (result != 0) {
             LOG_ERR("Error %d sending reply to message from 0x%d", result, address.ext_id);
@@ -85,7 +85,8 @@ void ThingSetZephyrCanInterface::onRequestResponseReceived(net_buf *buffer, int 
     }
 }
 
-void ThingSetZephyrCanInterface::AddressDiscoverer::onAddressClaimReceived(const struct device *dev, struct can_frame *frame, void *arg)
+void ThingSetZephyrCanInterface::AddressDiscoverer::onAddressClaimReceived(const struct device *dev,
+                                                                           struct can_frame *frame, void *arg)
 {
     AddressDiscoverer *self = (AddressDiscoverer *)arg;
     auto canId = CanID::create(frame->id);
@@ -96,7 +97,8 @@ void ThingSetZephyrCanInterface::AddressDiscoverer::onAddressClaimReceived(const
     // TODO: callback for claim forwarding
 }
 
-void ThingSetZephyrCanInterface::AddressDiscoverer::onAddressDiscoverSent(const struct device *dev, int error, void *arg)
+void ThingSetZephyrCanInterface::AddressDiscoverer::onAddressDiscoverSent(const struct device *dev, int error,
+                                                                          void *arg)
 {
     AddressDiscoverer *self = (AddressDiscoverer *)arg;
     if (error == 0) {
@@ -105,20 +107,18 @@ void ThingSetZephyrCanInterface::AddressDiscoverer::onAddressDiscoverSent(const 
 }
 
 static void onRequestResponseError(int8_t error, isotp_fast_addr addr, void *arg)
-{
-}
+{}
 
 static void onRequestResponseSent(int result, isotp_fast_addr addr, void *arg)
-{
-}
+{}
 
 bool ThingSetZephyrCanInterface::tryClaimAddress(uint8_t nodeAddress)
 {
     CanFrame frame(CanID()
-        .setSource(nodeAddress)
-        .setTarget(CanID::broadcastAddress)
-        .setMessageType(MessageType::network)
-        .setMessagePriority(MessagePriority::networkManagement));
+                       .setSource(nodeAddress)
+                       .setTarget(CanID::broadcastAddress)
+                       .setMessageType(MessageType::network)
+                       .setMessagePriority(MessagePriority::networkManagement));
     memcpy(frame.getData(), Eui::getArray().data(), Eui::getArray().size());
     frame.setLength(Eui::getArray().size());
     return can_send(_canDevice, frame.getFrame(), K_MSEC(10), nullptr, nullptr) == 0;
@@ -128,44 +128,47 @@ bool ThingSetZephyrCanInterface::bind(uint8_t nodeAddress)
 {
     if (_nodeAddress == CanID::broadcastAddress) {
         LOG_INF("Starting address claim for CAN interface %s", _canDevice->name);
-        AddressDiscoverer claimer { .nodeAddress = nodeAddress };
+        AddressDiscoverer claimer{ .nodeAddress = nodeAddress };
         k_event_init(&claimer.events);
 
         can_set_mode(_canDevice, CAN_MODE_FD);
         can_start(_canDevice);
-        auto addressClaimId = CanID().setMessageType(MessageType::network)
-            .setTarget(CanID::broadcastAddress)
-            .setBridge(0);
+        auto addressClaimId =
+            CanID().setMessageType(MessageType::network).setTarget(CanID::broadcastAddress).setBridge(0);
         can_filter addressClaimFilter = {
             .id = addressClaimId,
             .mask = addressClaimId.getMask(),
-            .flags = CAN_FILTER_IDE | CAN_FILTER_DATA,
+            .flags = CAN_FILTER_IDE
+#ifdef CAN_FILTER_DATA
+                     | CAN_FILTER_DATA
+#endif
+            ,
         };
-        int filterId = can_add_rx_filter(_canDevice, AddressDiscoverer::onAddressClaimReceived, &claimer, &addressClaimFilter);
+        int filterId =
+            can_add_rx_filter(_canDevice, AddressDiscoverer::onAddressClaimReceived, &claimer, &addressClaimFilter);
         CanFrame txFrame;
         while (1) {
             k_event_clear(&claimer.events, AddressClaimEvent::alreadyUsed);
 
             /* send out address discovery frame */
             uint8_t rand = sys_rand32_get() & 0xFF;
-            txFrame.setId(CanID().setMessageType(MessageType::network)
-                .setMessagePriority(MessagePriority::networkManagement)
-                .setRandomElement(rand)
-                .setTarget(claimer.nodeAddress)
-                .setSource(CanID::anonymousAddress));
-            int err =
-                can_send(_canDevice, txFrame.getFrame(), K_MSEC(10), AddressDiscoverer::onAddressDiscoverSent, &claimer);
+            txFrame.setId(CanID()
+                              .setMessageType(MessageType::network)
+                              .setMessagePriority(MessagePriority::networkManagement)
+                              .setRandomElement(rand)
+                              .setTarget(claimer.nodeAddress)
+                              .setSource(CanID::anonymousAddress));
+            int err = can_send(_canDevice, txFrame.getFrame(), K_MSEC(10), AddressDiscoverer::onAddressDiscoverSent,
+                               &claimer);
             if (err != 0) {
                 k_sleep(K_MSEC(100));
                 continue;
             }
 
-            uint32_t event =
-                k_event_wait(&claimer.events, AddressClaimEvent::alreadyUsed, false, K_MSEC(500));
+            uint32_t event = k_event_wait(&claimer.events, AddressClaimEvent::alreadyUsed, false, K_MSEC(500));
             if (event & AddressClaimEvent::alreadyUsed) {
                 /* try again with new random node_addr between 0x01 and 0xFD */
-                claimer.nodeAddress = CanID::minAddress
-                    + sys_rand32_get() % (CanID::maxAddress - CanID::minAddress);
+                claimer.nodeAddress = CanID::minAddress + sys_rand32_get() % (CanID::maxAddress - CanID::minAddress);
                 LOG_WRN("Node addr already in use, trying 0x%.2X", claimer.nodeAddress);
             }
             else {
@@ -181,14 +184,13 @@ bool ThingSetZephyrCanInterface::bind(uint8_t nodeAddress)
                 can_get_state(_canDevice, NULL, &err_cnt_after);
 
                 if (err_cnt_after.tx_err_cnt <= err_cnt_before.tx_err_cnt) {
-                    LOG_INF("Using CAN node address 0x%.2X on %s", claimer.nodeAddress,
-                            _canDevice->name);
+                    LOG_INF("Using CAN node address 0x%.2X on %s", claimer.nodeAddress, _canDevice->name);
                     break;
                 }
 
                 /* Continue the loop in the very unlikely case of a collision because two nodes with
-                * different EUI-64 tried to claim the same node address at exactly the same time.
-                */
+                 * different EUI-64 tried to claim the same node address at exactly the same time.
+                 */
             }
         }
         can_remove_rx_filter(_canDevice, filterId);
@@ -205,15 +207,16 @@ bool ThingSetZephyrCanInterface::listen(std::function<int(uint8_t *, size_t, uin
 #pragma GCC diagnostic push
 #pragma GCC diagnostic ignored "-Wnarrowing"
     isotp_fast_addr rxAddress = {
-        .ext_id = CanID().setMessageType(MessageType::requestResponse)
-            .setMessagePriority(MessagePriority::channel)
-            .setTarget(_nodeAddress),
+        .ext_id = CanID()
+                      .setMessageType(MessageType::requestResponse)
+                      .setMessagePriority(MessagePriority::channel)
+                      .setTarget(_nodeAddress),
     };
 #pragma GCC diagnostic pop
     _listener.callback = callback;
-    return isotp_fast_bind(&_listener.requestResponseContext, _canDevice, rxAddress,
-        &flowControlOptions, onRequestResponseReceived, &_listener, onRequestResponseError,
-        onRequestResponseSent) == 0;
+    return isotp_fast_bind(&_listener.requestResponseContext, _canDevice, rxAddress, &flowControlOptions,
+                           onRequestResponseReceived, &_listener, onRequestResponseError, onRequestResponseSent)
+           == 0;
 }
 
 } // namespace ThingSet::Can::Zephyr
