00a891c2c8f1fc4cf1df806699d7932b5a41f548
Adam Mitchell <adamstuartmitchell@gmail.com>
2025-04-30 13:54:53 +0000

    Mostly implement the client transport

--- include/thingset++/zsock/ThingSetZephyrSocketClientTransport.hpp
+++ include/thingset++/zsock/ThingSetZephyrSocketClientTransport.hpp
@@ -8,27 +8,35 @@
 #include "thingset++/ThingSetClientTransport.hpp"
 #include <cstdint>
 #include <cstdio>
+#include <zephyr/net/net_if.h>
 #include <zephyr/net/socket.h>
 
 namespace ThingSet::Zsock {
 
-class ThingSetZephyrSocketClientTransport
+class ThingSetZephyrSocketClientTransport : ThingSetClientTransport
 {
     private:
-        struct sockaddr_in _servaddr;
-        struct sockaddr_in _cli;
+        struct sockaddr_in _nodeaddr;
+        struct sockaddr_in _clientaddr;
 
         int _sock;
         int _opt;
         int _optlen;
 
     public:
-        ThingSetZephyrSocketClientTransport(const char *ip, uint16_t port);
+        ThingSetZephyrSocketClientTransport(struct net_if *iface, const char *ip, uint16_t port);
 
-        // bool connect() override;
-        // int read(uint8_t *buffer, size_t len) override;
-        // bool write(uint8_t *buffer, size_t len) override;
-        // void subscribe(std::function<void(uint8_t *, size_t)> callback) override;
+        bool connect() override;
+        int read(uint8_t *buffer, size_t len) override;
+        bool write(uint8_t *buffer, size_t len) override;
+        void subscribe(std::function<void(uint8_t *, size_t)> callback) override;
+
+        /// @brief Must be called before connect().
+        /// @param ip A null-terminated array containing the IP address of the device to which
+        ///           to connect.
+        /// @param port The port of the service running on the device to which to connect.
+        /// @return True if the connection parameters were successfully set.
+        bool set_client_connection_params(const char *ip, uint16_t port);
 };
 
 } // namespace ThingSet::Zsock
--- src/zsock/ThingSetZephyrSocketClientTransport.cpp
+++ src/zsock/ThingSetZephyrSocketClientTransport.cpp
@@ -4,47 +4,64 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 #include "thingset++/zsock/ThingSetZephyrSocketClientTransport.hpp"
-#include <cstring>
+#include <assert.h>
 #include <zephyr/net/net_if.h>
 #include <zephyr/net/net_ip.h>
 #include <zephyr/net/socket.h>
 
 namespace ThingSet::Zsock {
 
-ThingSetZephyrSocketClientTransport::ThingSetZephyrSocketClientTransport(const char *ip, uint16_t port)
+ThingSetZephyrSocketClientTransport::ThingSetZephyrSocketClientTransport(struct net_if *iface, const char *ip, uint16_t port)
     : _sock(-1), _opt(1), _optlen(sizeof(_opt))
 {
-    std::memset(&_servaddr, 0, sizeof(_servaddr));
-    std::memset(&_cli, 0, sizeof(_cli));
+    _nodeaddr.sin_family = AF_INET;
+    _nodeaddr.sin_port = htons(port);
 
-    int ret = 0;
-    char hr_addr[NET_IPV4_ADDR_LEN];
+    int ret = net_addr_pton(AF_INET, ip, &_nodeaddr.sin_addr);
 
-    struct net_if *iface = net_if_get_default();
-    
-    _servaddr.sin_family = AF_INET;
-    _servaddr.sin_port = htons(port);
-    ret = net_addr_pton(AF_INET, ip, &_servaddr.sin_addr);
-
-    net_if_ipv4_addr_add(iface, &_servaddr.sin_addr, NET_ADDR_MANUAL, 0);
-
-	printk("IPv4 address: %s\n",
-		net_addr_ntop(AF_INET, &_servaddr.sin_addr, hr_addr,
-			      NET_IPV4_ADDR_LEN));
-
-    printk("RET: %d, %d\n", ret, errno);
+    struct net_if_addr *if_addr = net_if_ipv4_addr_add(iface, &_nodeaddr.sin_addr,
+                                                       NET_ADDR_MANUAL, 0);
 
     _sock = zsock_socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
 
-    ret = zsock_getsockopt(_sock, IPPROTO_IP, SOL_SOCKET, &_opt, (socklen_t *)&_optlen);
-    printk("RET: %d, %d\n", ret, errno);
-    ret = zsock_bind(_sock, (struct sockaddr *)&_servaddr, sizeof(_servaddr));
+    __ASSERT(ret == 0, "Failed to convert %s to IP address: %d", ip, errno);
+    __ASSERT(if_addr != NULL, "Failed to add address to interface: %d", errno);
+    __ASSERT(_sock >= 0, "Failed to create socket: %d", errno);
+}
 
-    printk("RET: %d, %d\n", ret, errno);
+bool ThingSetZephyrSocketClientTransport::set_client_connection_params(const char *ip, uint16_t port)
+{
+    _clientaddr.sin_family = AF_INET;
+    _clientaddr.sin_port = htons(port);
+    
+    return net_addr_pton(AF_INET, ip, &_clientaddr.sin_addr) == 0;
+}
 
-    ret = zsock_listen(_sock, 1);
+bool ThingSetZephyrSocketClientTransport::connect()
+{
+    return zsock_connect(_sock, (struct sockaddr *)&_clientaddr, sizeof(_clientaddr)) == 0;
+}
 
-    printk("RET: %d, %d\n", ret, errno);
+int ThingSetZephyrSocketClientTransport::read(uint8_t *buffer, size_t len)
+{
+    return zsock_recv(_sock, buffer, len, 0);
+}
+
+bool ThingSetZephyrSocketClientTransport::write(uint8_t *buffer, size_t len)
+{
+    return zsock_send(_sock, buffer, len, 0) == (ssize_t)len;
+}
+
+void ThingSetZephyrSocketClientTransport::subscribe(std::function<void(uint8_t *, size_t)> callback)
+{
+    // asio::ip::udp::endpoint localEndpoint(asio::ip::address_v4::any(), 9002);
+    // asio::error_code error;
+    // _subscribeSocket.bind(localEndpoint, error);
+    // if (error) {
+    //     throw std::system_error(error);
+    // }
+    // co_spawn(_ioContext, listener(callback), detached);
+    return;
 }
 
 } // namespace ThingSet::Zsock
