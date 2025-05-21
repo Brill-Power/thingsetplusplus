ab721b82751811cc63fc43b073aeb68508f1e338
Gareth Potter <gareth.potter@brillpower.com>
2025-05-21 12:34:13 +0100

    fix

--- examples/server/main.cpp
+++ examples/server/main.cpp
@@ -99,7 +99,7 @@ int main()
     asio::steady_timer t(ioContext, asio::chrono::seconds(1));
     t.async_wait(std::bind(publishCallback, asio::placeholders::error, &t, &server));
 
-    server.start();
+    server.listen();
     ioContext.run();
     return 0;
 }
