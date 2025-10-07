5455ff0d32686328dad8e04ed134c2e575645250
Adam Mitchell <adam.mitchell@brillpower.com>
2025-10-07 13:51:52 +0000

    Revert "Separate queues for control and report data"
    This reverts commit f47dfc3b110e6d5ff6098b6c8fde0018a1b31149.

--- include/thingset++/can/zephyr/ThingSetZephyrCanSubscriptionTransport.hpp
+++ include/thingset++/can/zephyr/ThingSetZephyrCanSubscriptionTransport.hpp
@@ -22,7 +22,6 @@ private:
         const device *const _canDevice;
         int _filterId;
         ThingSet::Zephyr::MessageQueue<can_frame, CONFIG_THINGSET_PLUS_PLUS_CAN_SUBSCRIPTION_QUEUE_DEPTH> _frameQueue;
-        ThingSet::Zephyr::MessageQueue<can_frame, CONFIG_THINGSET_PLUS_PLUS_CAN_SUBSCRIPTION_QUEUE_DEPTH> _controlQueue;
         k_thread _thread;
         std::function<void(const CanID &, ThingSetBinaryDecoder &)> _callback;
 
@@ -33,13 +32,11 @@ private:
         bool run(std::function<void(const CanID &, ThingSetBinaryDecoder &)> callback, bool singleFrame);
 
     private:
-        static void runListener(void *param1, void *param2, void *);
-        void runListener(ThingSet::Zephyr::MessageQueue<can_frame, CONFIG_THINGSET_PLUS_PLUS_CAN_SUBSCRIPTION_QUEUE_DEPTH> &_queue);
+        static void runListener(void *param1, void *, void *);
+        void runListener();
 
         static void onPublicationFrameReceived(const device *canDevice, can_frame *frame, void *userData);
         void onPublicationFrameReceived(can_frame *frame);
-        static void onControlFrameReceived(const device *canDevice, can_frame *frame, void *userData);
-        void onControlFrameReceived(can_frame *frame);
 
     };
 
