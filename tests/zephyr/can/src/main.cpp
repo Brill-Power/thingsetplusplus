986787d7405f2478ce0165cd16ef4522f6bc2416
Gareth Potter <gareth.potter@brillpower.com>
2026-01-20 19:05:32 +0000

    too clever for zephyr

--- tests/zephyr/can/src/main.cpp
+++ tests/zephyr/can/src/main.cpp
@@ -100,14 +100,14 @@ ZCLIENT_SERVER_TEST(test_get_float,
 ZCLIENT_SERVER_TEST(test_exec_function,
     int value;
     auto result = client.exec(0x1000, &value, 2, 3);
-    zassert_true(result);
+    zassert_true(result.success());
     zassert_equal(ThingSetStatusCode::changed, result.code());
     zassert_equal(5, value);
 )
 
 ZCLIENT_SERVER_TEST(test_update,
     auto result = client.update("totalVoltage", 25.0f);
-    zassert_true(result);
+    zassert_true(result.success());
     zassert_equal(ThingSetStatusCode::changed, result.code());
     k_sleep(K_MSEC(100)); // `update` is async or something
     zassert_equal(25.0f, totalVoltage.getValue());
